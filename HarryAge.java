7e9113195eef83511da160fb1b80ecbd97eb335b
paaarthsharma <113705407+paaarthsharma@users.noreply.github.com>
2025-09-23 12:14:53 +0530

    Add files via upload

---
+++ HarryAge.java
@@ -0,0 +1,8 @@
+class HarryAge {
+    public static void main(String[] args) {
+        int harrybirthyear = 2000;
+        int currentYear = 2024;
+        int harryAge = currentYear - harrybirthyear;
+        System.out.println("Harry's age is: " + harryAge);
+    }
+}
